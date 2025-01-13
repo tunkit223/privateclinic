3e8c37ac4e445ec65ac5c26847cbef11d79254cb
Kiệt Trương Tuấn <tuankietwork22@gmail.com>
2025-01-13 21:00:56 +0700

    finish HomePage

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,62 +3,88 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
- 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+import { Form } from "@/components/ui/form"
+import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/router"
+
+
+export enum FormFieldTypes {
+  INPUT = 'input'
+  TEXTAREA = 'textarea'
+  PHONE_INPUT = 'phoneInput'
+  CHECKBOX = 'checkbox'
+  DATE_PICKER = 'datePicker'
+  SELECT = 'select'
+  SKELETON = 'skeleton'
+}
  
 const PatientForm = () => {
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  //const router = useRouter();
+  const [isLoading, setisLoading] = useState(false);
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   })
  
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
 
-    console.log(values)
+    setisLoading(true);
+
+    // try {
+    //     const userData ={ name, email, phone };
+
+    //     const user = await CreateUser(userData);
+
+    //     if(user) router.push(`/patient/${user.id}/register`);
+    // } catch (error) {
+    //   console.error(error);
+    // }
   }
   return (
     <Form {...form}>
     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
       <section className="mb-12 space-y-4">
-          <h1>Hi there👋</h1>
-          <p>Schedule your first appointment.</p>
+          <h1 className="header">Hi there👋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
       </section>
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
+      
+      <CustomFormField
+        filedType = {FormFieldTypes.INPUT}
+        control = {form.control}
+        name = 'name'
+        label = 'Full name'
+        placeholder = 'キエト'
+        iconSrc = '/assets/icons/user.svg'
+        iconAlt = 'user'
       />
-      <Button type="submit">Submit</Button>
+      
+      <CustomFormField
+        filedType = {FormFieldTypes.INPUT}
+        control = {form.control}
+        name = 'email'
+        label = 'Email'
+        placeholder = 'tunkit223@gmail.com'
+        iconSrc = '/assets/icons/email.svg'
+        iconAlt = 'email'
+      />
+
+      <CustomFormField
+        filedType = {FormFieldTypes.PHONE_INPUT}
+        control = {form.control}
+        name = 'phone'
+        label = 'Phone number'
+        placeholder = '(84+) 913 834 393'
+      />
+
+      <SubmitButton isLoading={isLoading}>Get started</SubmitButton>
     </form>
   </Form>
   )
---
+++ lib/validation.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const UserFormValidation = z.object({
+  name: z.string()
+    .min(1, "Username must be at least 1 characters.")
+    .max(50, "Username must be at most 2 characters."),
+  email: z.string().email("Invalid email address."),
+  phone: z.string().refine((phone)=>/^\d{10}$/.test(phone), 'Invalid phone number'),
+})
+ 
