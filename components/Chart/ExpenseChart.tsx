f8e02c009ef0d05cdfb4c64e9000200aba373011
KietTuan <theonlytruth25012005@gmail.com>
2025-04-26 10:44:29 +0700

    fix loi chu thich do thi expense

--- components/Chart/ExpenseChart.tsx
+++ components/Chart/ExpenseChart.tsx
@@ -87,8 +87,9 @@ export function ExpenseChart() {
         </ChartContainer>
         <div className="grid grid-cols-2 gap-4 px-4 py-2">
           {expenseDetails.map((item) => (
-            <div key={item.name} className="flex items-center space-x-2 ">
-              <span className={`w-3 h-3 rounded-full bg-[${item.color}]`}></span>
+            <div key={item.name} className="flex items-center space-x-2">
+              {/* <span className={`w-3 h-3 rounded-full bg-[${item.color}]`}></span> */}
+              <span className="w-3 h-3 rounded-full flex-shrink-0" style={{ backgroundColor: item.color }}></span>
               <div>
                 <p className="text-sm text-muted-foreground">{item.name}</p>
                 <p className="text-base font-semibold">${item.value.toLocaleString()}</p>
